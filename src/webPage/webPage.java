08106676f02015b17834632b0c911fd1ccc604b3
ft7700 <57183888+ft7700@users.noreply.github.com>
2019-12-21 12:49:38 +0800

    Add files via upload

--- src/webPage/webPage.java
+++ src/webPage/webPage.java
@@ -1,33 +1,31 @@
-package webPage;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package driversearch;
 
 /**
  *
- * @author nakamurakaito
+ * @author FT
  */
-public class webPage implements Comparable<webPage> {
+public class webPage implements Comparable<webPage>{
+    private String first; // first name
+    private String last;  // last name
+  
     private String url,title,ipAddress;
     private int protal;
     
-    public webPage(){
-        
-    }
+    public webPage() {
+    first = "";
+    last = "";
+  } // end default constructor
     
     public webPage(String url, String title,String ipAddress){
         this.url = url;
         this.ipAddress = ipAddress;
         this.title = title;
         this.protal = protal;
-    }
-
-    @Override
-    public int compareTo(webPage o) {
-        return url.compareTo(o.url);
     }
 
     public String getUrl() {
@@ -45,4 +43,83 @@ public class webPage implements Comparable<webPage> {
     public int getProtal() {
         return protal;
     }
-}
+
+  
+
+  public webPage(String firstName) { // for testing *************** 
+    setName(firstName, "");
+  } // end constructor
+
+  public webPage(String firstName, String lastName) {
+    first = firstName;
+    last = lastName;
+  } // end constructor
+
+  public int hashCode() { // for testing *************** 
+    // this hash code causes collisions
+    int h = 0;
+
+    for (int i = 0; i < first.length(); i++) {
+      h = h + first.charAt(i);
+    }
+    return h;
+  } // end hashCode
+
+  public void setName(String firstName, String lastName) {
+    setFirst(firstName);
+    setLast(lastName);
+  } // end setName
+
+  public String getName() {
+    return toString();
+  } // end getName
+
+  public void setFirst(String firstName) {
+    first = firstName;
+  } // end setFirst
+
+  public String getFirst() {
+    return first;
+  } // end getFirst
+
+  public void setLast(String lastName) {
+    last = lastName;
+  } // end setLast
+
+  public String getLast() {
+    return last;
+  } // end getLast
+
+  public void giveLastNameTo(webPage aName) {
+    aName.setLast(last);
+  } // end giveLastNameTo
+
+  public String toString() {
+    return first + " " + last;
+  } // end toString
+
+  public boolean equals(Object other) {
+    boolean result;
+
+    if ((other == null) || (getClass() != other.getClass())) {
+      result = false;
+    } else {
+      webPage otherName = (webPage) other;
+      result = first.equals(otherName.first) && last.equals(otherName.last);
+    } // end if
+
+    return result;
+  } // end equals
+
+  public int compareTo(webPage otherName) {
+    int result = last.compareTo(otherName.last);
+
+    // if last names are equal, check first names
+    if (result == 0) {
+      result = first.compareTo(otherName.first);
+    }
+
+    return result;
+  } // end compareTo
+} // end Name
+
