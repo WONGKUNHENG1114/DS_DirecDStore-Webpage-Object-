1550f556e0ad8160e7f92ee7d0634c46f9084e0c
KOKMENKIT <56879028+KOKMENKIT@users.noreply.github.com>
2019-12-22 16:50:56 +0800

    Add files via upload

--- Algorithm(TreeSearch)/src/algorithm/treesearch/TreeSearch.java
+++ Algorithm(TreeSearch)/src/algorithm/treesearch/TreeSearch.java
@@ -85,45 +85,7 @@ public class TreeSearch<T extends Comparable> implements TreeSearchADT<T> {
        }
         return result;
     }
-    
-    public void MorrisTraversal(Node root) 
-    { 
-        Node current, pre; 
-  
-        if (root == null) 
-            return; 
-  
-        current = root; 
-        while (current != null) { 
-            if (current.left == null) { 
-                System.out.print(current.data + " "); 
-                current = current.right; 
-            } 
-            else { 
-                /* Find the inorder predecessor of current */
-                pre = current.left; 
-                while (pre.right != null && pre.right != current) 
-                    pre = pre.right; 
-  
-                /* Make current as right child of its inorder predecessor */
-                if (pre.right == null) { 
-                    pre.right = current; 
-                    current = current.left; 
-                } 
-  
-                /* Revert the changes made in the 'if' part to restore the  
-                    original tree i.e., fix the right child of predecessor*/
-                else { 
-                    pre.right = null; 
-                    System.out.print(current.data + " "); 
-                    current = current.right; 
-                } /* End of if condition pre->right == NULL */
-  
-            } /* End of if condition current->left == NULL*/
-  
-        } /* End of while */
-    }
-    
+     
     public class Node {
         private T data;
         private Node left;
