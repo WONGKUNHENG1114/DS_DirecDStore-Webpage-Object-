bc41e4ea052bba80584e721c916e21c039126761
ft7700 <57183888+ft7700@users.noreply.github.com>
2019-12-22 20:56:03 +0800

    Add files via upload

--- src/webPage/webPage.java
+++ src/webPage/webPage.java
@@ -13,7 +13,7 @@ public class webPage implements Comparable<webPage>{
     private String first; 
     private String last;  
   
-    private String url,title,ipAddress,date;
+    private String url,title,ipAddress;
     private int protal;
     
     public webPage() {
@@ -25,14 +25,6 @@ public class webPage implements Comparable<webPage>{
         this.url = url;
         this.ipAddress = ipAddress;
         this.title = title;
-        this.protal = protal;
-    }
-    
-    public webPage(String title, String url,String ipAddress, String date){
-        this.url = url;
-        this.ipAddress = ipAddress;
-        this.title = title;
-        this.date = date;
         this.protal = protal;
     }
 
@@ -63,14 +55,15 @@ public class webPage implements Comparable<webPage>{
     last = lastName;
   } 
   
-  public int hashCode() { 
-    int h = 0;
+
+  public int hashCode() {
+     int h = 0;
 
     for (int i = 0; i < first.length(); i++) {
-      h = h + first.charAt(i);
+    h = 31 * h + first.charAt(i);
     }
     return h;
-  } 
+  }
 
   public void setName(String firstName, String lastName) {
     setFirst(firstName);
@@ -105,28 +98,27 @@ public class webPage implements Comparable<webPage>{
     return first + " " + last;
   } 
 
-  public boolean equals(Object other) {
+  public boolean equals(Object obj) {
     boolean result;
 
-    if ((other == null) || (getClass() != other.getClass())) {
+    if ((obj == null) || (getClass() != obj.getClass())) {
       result = false;
     } else {
-      webPage otherName = (webPage) other;
+      webPage otherName = (webPage) obj;
       result = first.equals(otherName.first) && last.equals(otherName.last);
     } 
 
     return result;
   } 
-
+  
   public int compareTo(webPage otherName) {
-    int result = last.compareTo(otherName.last);
+    int res = last.compareTo(otherName.last);
 
-    // if last names are equal, check first names
-    if (result == 0) {
-      result = first.compareTo(otherName.first);
+    if (res == 0) {
+      res = first.compareTo(otherName.first);
     }
 
-    return result;
+    return res;
   } 
 } 
 
